44e6e7c72f5ddcd05f03826130763fbb9af61be3
Andris Reinman <andris@kreata.ee>
2016-08-04 12:00:27 +0300

    v1.1.0. Added method getByTime

--- README.md
+++ README.md
@@ -28,6 +28,26 @@ var id = indexer.get();
 // 15654036c1709f68f2
 ```
 
+### short()
+
+Generate a sequential ID without instance suffix using `short()`
+
+```javascript
+var shortId = indexer.short();
+// 15654036c1709f
+```
+
+### getByTime()
+
+Use to convert a timestamp into a short ID for comparison
+
+```javascript
+var shortId = indexer.getByTime(1470300906242, 0);
+// 15654c36302000
+var id = shortId + indexer.suffix;
+// 15654c3630200068f2
+```
+
 ## License
 
 **MIT**
--- test/seq-index-test.js
+++ test/seq-index-test.js
@@ -14,3 +14,18 @@ module.exports['Generate unique sequential IDs'] = test => {
 
     test.done();
 };
+
+module.exports['Generate specific ID'] = test => {
+    let time = 1470300906242;
+    let seq = 100;
+    let s = new SeqIndex();
+    let last = s.getByTime(time, seq);
+
+    for (let i = 0; i < 100; i++) {
+        let next = s.getByTime(time, seq);
+        test.equal(next.localeCompare(last), 0, 'Generated index must be higher than previous value');
+        last = next;
+    }
+
+    test.done();
+};
